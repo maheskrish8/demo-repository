703573b41bf82750984edc37a46626f1ccaf1100
maheskrish8 <167216526+maheskrish8@users.noreply.github.com>
2024-04-17 20:00:44 +0530

    demo branch commit

--- sample.py
+++ sample.py
@@ -5,8 +5,13 @@ def add(a,b):
 def sub(x,y):
     z=x-y
     print(z)
+
+def mul(a,b,c):
+    d = a*b*c;
+    print(d)
     
 if __name__=="__main__":
   add(10,5)
   sub(100,50)
+  mul(10,20,30)
   
